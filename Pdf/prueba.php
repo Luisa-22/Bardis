4274a2ae22d612b5ab3daf9bad3d8dcd0f20a3e0
Jerson Misas <camilomisas0@gmail.com>
2020-04-01 00:59:06 -0500

    Diseño a la factura

--- Pdf/prueba.php
+++ Pdf/prueba.php
@@ -11,24 +11,24 @@ function Header()
             // Arial bold 15
             $this->SetFont('Arial','',13);
             // Move to the right
-            $this->Cell(70);
+            $this->Cell(100);
             // Title
             $this->Cell(50,10,'Alma Bendita Fonda-Bar',0,'','C');
             $this->SetFont('Arial','',10);
             $this->Ln(15);
-            $this->Cell(60);
+            $this->Cell(90);
             $this->Cell(50,10,'NIT: 990456-1');
             $this->Ln(10);
-            $this->Cell(64);
+            $this->Cell(90);
             $this->Cell(50,10,'300 3063080');
             $this->Ln(10);
-            $this->Cell(55);
+            $this->Cell(85);
             $this->Cell(50,10,'Cra. 70#46B-58');
             // Line break
             $this->Ln(10);
             $this->SetFont('Arial','B',10);
             $this->Cell(-23);
-            $this->Cell(88,60,'Producto',0,'','L');
+            $this->Cell(130,60,'Producto',0,'','L');
             $this->Cell(50,60,'Precio  ',0,'','C');
             $this->cell(50,60,'Cantidad',0,'','C');
             $this->Cell(50,60,'Sub total',0,'','C');
@@ -38,7 +38,7 @@ function Header()
     include '../php/conexion.php';
     $registro=mysqli_query($conexion,"SELECT numero_mesa,id_pedido,total,id_estado FROM tbl_pedido WHERE id_pedido=$idDelPedido") or die ('error con el dato');
     while($rowproducto =$registro->fetch_assoc()){
-    $pdf = new PDF('P','pt',array(350,249));
+    $pdf = new PDF('P','pt',array(350,300));
     $pdf->AddPage();
     $pdf->SetFont('Arial','',10);
     $idPedido = $rowproducto['id_pedido'];
@@ -50,7 +50,7 @@ function Header()
     while($rowPro=mysqli_fetch_array($selectP)){
         
     $pdf->Cell(-23);
-    $pdf->Cell(88,60,$rowPro['nombre_producto'],0,'','L');
+    $pdf->Cell(130,60,$rowPro['nombre_producto'],0,'','L');
     $pdf->Cell(50,60,$rowPro['precio_producto'],0,'','C');
     $pdf->Cell(50,60,$producto['cantidad'],0,'','C');
     $subtotal = $rowPro['precio_producto'] * $producto['cantidad'];
@@ -61,7 +61,7 @@ function Header()
     
     $pdf->Ln(50);
     $pdf->cell(-23);
-    $pdf->cell(190,50,'Total',0,0,'L',0);
+    $pdf->cell(230,50,'Total',0,0,'L',0);
     $pdf->SetFont('Arial','',10);
     $pdf->cell(50,50,"$".$rowproducto['total'],0,0,'C','');
 }
