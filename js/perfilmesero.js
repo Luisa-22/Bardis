b6181741163507bb4559f0b31cff5c6ad9913abf
Jerson Misas <camilomisas0@gmail.com>
2020-05-04 19:42:34 -0500

    Update perfilmesero.js
    Se documenta las funciones del menú del perfil de mesero.

--- js/perfilmesero.js
+++ js/perfilmesero.js
@@ -1,10 +1,33 @@
+/**
+ * Se crea una función llamada submenu con la cual se perimitirá hacer que el menú visible para el usuario
+ */
 function submenu(){
+  /**
+   * se obtiene el elemento de id 'section-opc' y se pone visible.
+  */
   document.getElementById('section-opc').style.display = "block";
+  /**
+   * se obtiene el elemento de id 'icon-2' que es un icono de una flecha hacia arriba y se pone visible.
+  */
   document.getElementById('icon-2').style.display = "inline-flex";
+  /**
+   * se obtiene el elemento de id 'icon-1' que es un icono de una flecha hacia abajo y se pone invisible.
+  */
   document.getElementById('icon-1').style.display = "none";
 }
-function quitarsubmenu(){
+/** 
+ * se crea la función quitarsubmenu para que se coulte el menú
+*/
+function quitarsubmenu(){/**
+  * se obtiene el elemento de id 'section-opc' y se pone invisible para el usuario.
+ */
   document.getElementById('section-opc').style.display = "none";
+  /**
+   * se obtiene el elemento de id 'icon-2' que es un icono de una flecha hacia arriba y se pone invisible para el usuario.
+  */
   document.getElementById('icon-2').style.display = "none";
+  /**
+   * se obtiene el elemento de id 'icon-1' que es un icono de una flecha hacia abajo y se pone visible para el usuario.
+  */
   document.getElementById('icon-1').style.display = "inline-flex";
 }
