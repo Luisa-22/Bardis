d1f3109271acbf5246ddcfbeed31889243d7df30
Jerson Misas <camilomisas0@gmail.com>
2020-05-04 19:38:59 -0500

    Update registro.php
    Inserté elemtos HTML para poder generar diferentes tipos de validaciones al formulario de registro del usuario

--- vistas/registro.php
+++ vistas/registro.php
@@ -27,14 +27,22 @@
     </div>
     <div id="formulario">
       <div class="div-formulario-registro">
-        <form id="formulario-1" action="../php/registrocliente.php" method="post">
+        <form id="formulario-1" action="../php/registrocliente.php" onsubmit="return validarRegistro()" method="post">
           <h3>Registro cliente</h3>
-          <input type="text" name="documentoCliente" onkeypress="return SoloNumeros(event)" onpaste="return false" placeholder="Documento" required>
-          <input type="text" name="nombreCliente" onkeypress="return SoloLetras(event)" onpaste="return false" placeholder="Nombre de usuario" required>
-          <input type="email" name="correoCliente" onkeypress="return SoloNumerosyLetras(event)" onpaste="return false" placeholder="Correo" required>
-          <input type="text" name="edadCliente" onkeypress="return SoloNumeros(event)" onpaste="return false" placeholder="Edad" required>
-          <input type="password" name="password" onkeypress="return SoloNumerosyLetras(event)" onpaste="return false" placeholder="Clave" required>
-          <input type="password" name="confirmarPassword" onkeypress="return SoloNumerosyLetras(event)" onpaste="return false" placeholder="Confirmar clave" required>
+          <h5 id="alert"></h5>
+          <input type="text" name="documentoCliente" id="documentoCliente" onkeypress="return SoloNumeros(event)" onpaste="return false" placeholder="Documento">
+          <h5 id="alertDocumento"></h5>
+          <input type="text" name="nombreCliente" id="nombreCliente" onkeypress="return SoloLetras(event)" onpaste="return false" placeholder="Nombre de usuario">
+          <h5 id="alertNombre"></h5>
+          <input type="email" name="correoCliente" id="correoCliente" onkeypress="return SoloNumerosyLetras(event)" onpaste="return false" placeholder="Correo">
+          <h5 id="alertCorreo"></h5>
+          <input type="text" name="edadCliente" id="edadCliente" onkeypress="return SoloNumeros(event)" onpaste="return false" placeholder="Edad">
+          <h5 id="alertEdad"></h5>
+          <input type="password" name="password" id="contrasena" onkeypress="return SoloNumerosyLetras(event)" onpaste="return false" placeholder="Clave" >
+          <h5 id="alertContrasena"></h5>
+          <input type="password" id="ConfirmarContrasena" name="confirmarPassword" id="confirmarContrasena" onkeypress="return SoloNumerosyLetras(event)" onpaste="return false" placeholder="Confirmar clave">
+          <h5 id="alertConfirmarContrasena"></h5>
+          <h5 id="alertContrasena2"></h5>
           <input type="submit" name="submitCliente">
         </form>
       </div>
